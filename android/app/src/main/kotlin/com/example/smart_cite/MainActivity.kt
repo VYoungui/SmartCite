ac9eecf3276693058183d42ecbdfcfe22fb77abf
Victoire Youngui <victoire.ntchuigwa@2026.ucac-icam.com>
2025-05-28 12:50:35 +0100

    initial commit

--- android/app/src/main/kotlin/com/example/smart_cite/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.smart_cite
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/core/environment.dart
+++
@@ -1,4 +0,0 @@
-class Environment {
-  static const String environment = String.fromEnvironment('env', defaultValue: 'dev');
-  static const String apiBaseUrl = String.fromEnvironment('API_BASE_URL');
-}
